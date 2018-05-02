7b7cfcacf1a73af57f9538cd63ac7af0e1a52afc
derrickdjoe <38145578+derrickdjoe@users.noreply.github.com>
2018-05-01 22:22:23 -0700

    Update pathing.cpp
    Added basic flood fill, still need to detect obstacle and add them to the grid

--- src/labpkg/src/pathing.cpp
+++ src/labpkg/src/pathing.cpp
@@ -13,6 +13,80 @@ using namespace std;
 bool shouldStop = false;
 actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> *ac;
 void sendNewGoal();
+
+int gridMap[20][20];
+geometry_msgs::Pose2D targetPose;
+
+void floodFillInit(){
+
+	for(int i = 0; i < 20; i++){
+
+		for(int j = 0; i < 20; j++){
+
+			gridMap[i][j] = 999;
+
+		}
+
+	}
+
+}
+
+void floodFillPlanner(int arr[][10], int x, int y){
+
+	//if visited every spot stop
+	int count = 0;
+
+	for(int i = 0; i < 20; i++){
+
+		for(int j = 0; j < 20; j++){
+
+			if(arr[i][j] == 0){
+
+				count++;
+
+			}
+
+		}
+
+	}
+
+	if(count == 400){
+		
+		ROS_INFO_STREAM("Done");
+		ac->cancelAllGoals();
+
+	}
+	//set current location to 0
+	arr[x][y] = 0;
+
+
+	if(arr[x + 1][y] == 999){
+
+		floodFillPlanner(arr, x + 1, y);
+		targetPose.x += 1;
+	
+	}else if(arr[x][y + 1] == 999){
+
+		floodFillPlanner(arr, x, y + 1);
+		targetPose.y += 1;
+
+	}else if(arr[x - 1][y] == 999){
+
+		floodFillPlanner(arr, x - 1, y);
+		targetPose.x -= 1;
+
+	}else if(arr[x][y - 1] == 999){
+
+		floodFillPlanner(arr, x, y - 1);
+		targetPose.y -= 1;
+
+	}
+
+	
+}	
+
+
+	
 void serviceActivated() {}
 void serviceFeedback(const move_base_msgs::MoveBaseFeedbackConstPtr& fb) {
 }
@@ -27,8 +101,8 @@ void sendNewGoal () {
     goal.target_pose.header.frame_id = "map";
     goal.target_pose.header.stamp = ros::Time::now();
     int targetPos = (rand() % 14) - 7;   
-    goal.target_pose.pose.position.x = targetPos;
-    goal.target_pose.pose.position.y = targetPos;
+    goal.target_pose.pose.position.x = targetPose;
+    goal.target_pose.pose.position.y = targetPose;
     goal.target_pose.pose.orientation.w = 1.0;
     //ROS_INFO_STREAM("Sending new goal: (" << goal.target_pose.pose.position.x << ", " << goal.target_pose.pose.position.y << ")\n");
     ros::Duration(0.1).sleep();
@@ -47,6 +121,7 @@ int main(int argc,char **argv) {
 	ros::init(argc,argv,"saferandomwalk");
     ros::NodeHandle nh;
 	ros::Subscriber scanSub = nh.subscribe("/scan", 1000, &updateScan);
+	//ros::Subscriber subLoc = nh.subscribe("/tf", 1000, &currentLoc);
 	
     ac = new actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>("move_base",true);
 	
